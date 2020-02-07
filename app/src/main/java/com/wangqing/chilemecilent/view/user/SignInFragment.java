d2d8cf5e4abb5bb10b192a3044909049b75a8c6c
wangqingzh <qingwang7962@gmail.com>
2020-02-07 22:28:47 +0800

    layout +

--- app/src/main/java/com/wangqing/chilemecilent/view/user/SignInFragment.java
+++ app/src/main/java/com/wangqing/chilemecilent/view/user/SignInFragment.java
@@ -3,12 +3,17 @@ package com.wangqing.chilemecilent.view.user;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.wangqing.chilemecilent.R;
 
 /**
@@ -29,4 +34,27 @@ public class SignInFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_sign_in, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        TextView textView = getActivity().findViewById(R.id.textViewSignUp);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController controller = Navigation.findNavController(v);
+                controller.navigate(R.id.action_signInFragment_to_signUpFragment);
+            }
+        });
+
+        getActivity().findViewById(R.id.textViewFindPassword).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController controller = Navigation.findNavController(v);
+                controller.navigate(R.id.action_signInFragment_to_findPasswordFragment);
+            }
+        });
+
+
+    }
 }
