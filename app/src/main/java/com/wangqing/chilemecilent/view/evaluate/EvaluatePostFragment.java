bb3026ba8843fb934e4cae11c84cd342a96a1e57
wangqingzh <qingwang7962@gmail.com>
2020-03-10 14:23:33 +0800

    code update

--- app/src/main/java/com/wangqing/chilemecilent/view/evaluate/EvaluatePostFragment.java
+++ app/src/main/java/com/wangqing/chilemecilent/view/evaluate/EvaluatePostFragment.java
@@ -2,18 +2,28 @@ package com.wangqing.chilemecilent.view.evaluate;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.wangqing.chilemecilent.R;
+import com.wangqing.chilemecilent.databinding.FragmentEvaluateBinding;
+import com.wangqing.chilemecilent.databinding.FragmentEvaluatePostBinding;
+import com.wangqing.chilemecilent.viewmodel.evaluate.EvaluatePostViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EvaluatePostFragment extends Fragment {
+
+    private FragmentEvaluatePostBinding binding;
+    private EvaluatePostViewModel viewModel;
+
 
     public EvaluatePostFragment() {
         // Required empty public constructor
@@ -23,7 +33,27 @@ public class EvaluatePostFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_evaluate_post, container, false);
+        viewModel = new ViewModelProvider(this).get(EvaluatePostViewModel.class);
+        binding.setData(viewModel);
+
+        binding.setLifecycleOwner(requireActivity());
+
+
+        return binding.getRoot();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_evaluate_post, container, false);
+        //return inflater.inflate(R.layout.fragment_evaluate_post, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/wangqing/chilemecilent/viewmodel/evaluate/EvaluatePostViewModel.java
@@ -0,0 +1,13 @@
+package com.wangqing.chilemecilent.viewmodel.evaluate;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class EvaluatePostViewModel extends AndroidViewModel {
+
+    public EvaluatePostViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
